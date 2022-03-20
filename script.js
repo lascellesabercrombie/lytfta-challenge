5510507e1a1c3426b6e4fee4bb51c2bb703eb0e8
Alex Grafen <alex.grafen@gmail.com>
2022-03-20 09:31:37 +0000

    function to generate gallery videos

---
+++ script.js
@@ -0,0 +1,50 @@
+const mainVideoArea = document.querySelector("#mainVideoArea");
+const galleryVideoArea = document.querySelector("#galleryVideoArea");
+
+const videos = [
+    {
+        title: `What is Lyfta?`,
+        image: "",
+        duration: 141,
+        source: `https://www.youtube.com/watch?v=9Z4zCtv3IEw&ab_channel=Lyfta`,
+    },
+    {
+        title: `Short interview with Lyfta's Co-CEO Serdar Ferit`,
+        image: "",
+        duration: 204,
+        source: `https://www.youtube.com/watch?v=1EDgKYfzzHI&ab_channel=Lyfta`
+    },
+    {
+        title: `Journey to a New Home - Trailer`,
+        image: "",
+        duration: 61,
+        source: `https://www.youtube.com/watch?v=DNcnmCYeCkk&ab_channel=Lyfta`
+    },
+    {
+        title: `Lyfta for children with autism and SEN`,
+        image: "",
+        duration: 125,
+        source: `https://www.youtube.com/watch?v=YoNvtEpzAoA&ab_channel=Lyfta`
+    },
+]
+
+function createGalleryVideo(video) {
+    console.log('x');
+    const {title, image, duration, source} = video;
+
+    const template = document.querySelector("#galleryVideoTemplate");
+
+    const domFragment = template.content.cloneNode(true);
+
+    domFragment.querySelector("h3").textContent = title;
+    if (image.length == 0) {
+    domFragment.querySelector("img").src = `https://yt3.ggpht.com/ytc/AKedOLR97_0kXyqk68f4TkZWd0OoS6zLNdbOmWpzqJlH=s176-c-k-c0x00ffffff-no-rj-mo`;
+    }
+    else {
+    domFragment.querySelector("img").src = image;
+    }
+   
+    galleryVideoArea.append(domFragment);
+}
+
+videos.forEach(createGalleryVideo);
