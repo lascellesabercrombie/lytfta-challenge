0a72153af3ec271b6139e754db0f598e6ef4af33
Alex Grafen <alex.grafen@gmail.com>
2022-03-20 20:13:14 +0000

    remove console logs and add comments at a couple points

--- script.js
+++ script.js
@@ -31,8 +31,6 @@ const videos = [
 let counter = 0;
 
 function createGalleryVideo(video) {
-    console.log('x');
-
     const {title, image, duration} = video;
 
     const template = document.querySelector("#galleryVideoTemplate");
@@ -41,12 +39,14 @@ function createGalleryVideo(video) {
     domFragment.querySelector("div").id = `gallery-video-${counter}`;
     domFragment.querySelector("div").addEventListener('click', selectVideo);
     domFragment.querySelector("h3").textContent = title;
+
+    //use Lyfta logo as thumbnail if no still provided
     if (image.length == 0) {
     domFragment.querySelector("img").src = `https://yt3.ggpht.com/ytc/AKedOLR97_0kXyqk68f4TkZWd0OoS6zLNdbOmWpzqJlH=s176-c-k-c0x00ffffff-no-rj-mo`;
-    }
-    else {
+    } else {
     domFragment.querySelector("img").src = image;
     }
+
     domFragment.querySelector("p").textContent = `${duration} seconds`;
     
     galleryVideoArea.appendChild(domFragment);
@@ -56,9 +56,10 @@ function createGalleryVideo(video) {
 videos.forEach(createGalleryVideo);
 
 function createMainVideo(video) {
-    console.log('y');
     const {title, source} = video;
+
     const template = document.querySelector("#mainVideoTemplate");
+
     const domFragment = template.content.cloneNode(true);
     domFragment.querySelector("h2").textContent = title;
     domFragment.querySelector("iframe").src = source;
@@ -67,13 +68,15 @@ function createMainVideo(video) {
 
 function selectVideo(e) {
     const currentVideo = mainVideoArea.querySelectorAll("div");
-
+    //remove video currently occupying main spot
     if (currentVideo.length > 0) {
         currentVideo.forEach(video => video.textContent = "");
     }
+    //use the numbered ID to pick the right video to replace it
     const targetId = e.target.parentNode.id;
     const targetNumber = targetId.match(/\d/)[0];
     createMainVideo(videos[targetNumber]);
 } 
 
+//initialise with first video as main video
 createMainVideo(videos[0]);
